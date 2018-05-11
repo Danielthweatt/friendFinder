0860ed350d3caffd32df27ea81deb6c6784a48e4
Danielthweatt <dthweatt192@yahoo.com>
2018-05-11 15:23:07 -0500

    continued working on API routing

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,11 @@ const friends = require("../data/friends");
 module.exports = function(app){
 
     app.get("/api/friends", function(req, res){
-        return res.json(friends);
-    });    
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res){
+        friends.push(req.body);
+    });
 
 };
