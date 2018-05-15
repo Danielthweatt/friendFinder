f9ca69c4351817be308563d9e20c58576f3393ef
Danielthweatt <dthweatt192@yahoo.com>
2018-05-15 00:42:02 -0500

    worked on logic for selecting friend match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,8 +11,31 @@ module.exports = function(app){
     });
 
     app.post("/api/friends", function(req, res){
+        req.body.scores = req.body.scores.map(function(score){
+            return parseInt(score);
+        });
+        const myTotalScore = req.body.scores.reduce(function(total, score){
+            return total + score;
+        });
+        const potentialFriends = [];
+        let potentialFriendsTotalScore;
+        friends.forEach(function(potentialFriend){
+            potentialFriendsTotalScore = potentialFriend.scores.reduce(function(total, score){
+                return total + score;
+            });
+            potentialFriends.push([potentialFriend.name, potentialFriend.photo, 
+            Math.abs(potentialFriendsTotalScore - myTotalScore)]);
+        });
         friends.push(req.body);
-        res.send(friends);
+        let match = ["", "", 41];
+        potentialFriends.forEach(function(potentialFriend){
+            if (match[2] > potentialFriend[2]) {
+                match[0] = potentialFriend[0];
+                match[1] = potentialFriend[1];
+                match[2] = potentialFriend[2];
+            }
+        });
+        res.send(match);
     });
 
 };
