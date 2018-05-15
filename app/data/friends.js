b5c12ce537000281dbfbcc48a55c6657ed9659e2
Danielthweatt <dthweatt192@yahoo.com>
2018-05-15 16:00:36 -0500

    some more finishing touches

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,7 +16,39 @@ friends = [
             4,
             4
         ]
-    }  
+    },
+    {
+        "name":"Michelle",
+        "photo":"http://ksassets.timeincuk.net/wp/uploads/sites/55/2017/07/Zendaya-920x584.png",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            5,
+            1,
+            1,
+            1,
+            1,
+            5
+        ]
+    },
+    {
+        "name":"Peter",
+        "photo":"https://i.pinimg.com/originals/ee/fd/dc/eefddcc7df87ab8e37633bafde6c14e7.jpg",
+        "scores":[
+            5,
+            5,
+            5,
+            3,
+            5,
+            5,
+            3,
+            5,
+            2,
+            5
+        ]
+    }    
 ];
 
 module.exports = friends;
